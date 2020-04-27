f8736e7f24d0795262449cbe3235dd8dcdaf0ef2
dmtG <dimitri.gevers@gmail.com>
2020-04-27 15:19:27 +0200

    Initial commit

---
+++ src/test/java/be/vdab/fietsacademy/FietsacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.fietsacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FietsacademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
