a0826f0d7cb19d32fc7cea66602d575bb1ea13f7
dmtG <dimitri.gevers@gmail.com>
2020-04-27 21:17:27 +0200

    p16

---
+++ src/main/java/be/vdab/fietsacademy/repositories/JpaDocentRepository.java
@@ -0,0 +1,33 @@
+package be.vdab.fietsacademy.repositories;
+
+import be.vdab.fietsacademy.domain.Docent;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.print.Doc;
+import java.util.Optional;
+
+@Repository
+public class JpaDocentRepository implements DocentRepository {
+    private final EntityManager entityManager;
+
+    public JpaDocentRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public Optional<Docent> findById(long id) {
+        return Optional
+                .ofNullable(
+                        entityManager
+                                .find(
+                                        Docent.class,
+                                        id
+                        )
+                );
+        //        throw new UnsupportedOperationException();
+    }
+
+
+
+}
---
+++ src/main/resources/insertDocent.sql
@@ -0,0 +1,2 @@
+insert into docenten(voornaam, familienaam, wedde, emailAdres)
+VALUES ('testM', 'testM', 1000, 'testM@test.be')
